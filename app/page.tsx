7007548d75b5acfab6571efb46420d548d95fa1c
Jahongirxon <xannazarovjahongir@gmail.com>
2024-10-05 14:00:40 +0500

    add irregular verbs audios

--- app/page.tsx
+++ app/page.tsx
@@ -13,21 +13,28 @@ type Unit = {
   unit_name: string;
   options: Option[];
 };
+type Irregular = {
+  id: number;
+  verb1: string;
+  verb2: string;
+  verb3: string;
+  verbUzb: string;
+}
 
 export default function Home() {
 
-  const [data, setData] = useState<Unit[] | null>(null);
+  const [data, setData] = useState<Irregular[] | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch('/essential4.json')
+    fetch('/irregular-verbs.json')
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.json();
       })
-      .then((jsonData: Unit[]) => setData(jsonData))
+      .then((jsonData: Irregular[]) => setData(jsonData))
       .catch((err) => setError(err.message));
   }, []);
 
@@ -36,19 +43,26 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className=' text-2xl '>{data[0].book_name}</p>
+      {/* <p className=' text-2xl '>{data[0].book_name}</p> */}
       <div className=' grid grid-cols-3 gap-3'>
         {data.map((unit) => (
-          <div className=' border-2 my-2 p-3' key={unit.unit_id}>
-            <h2>{unit.unit_name}</h2>
+          <div className=' border-2 my-2 p-3' key={unit.id}>
+            {/* <h2>{unit.unit_name}</h2> */}
             <ul>
-              {unit.options.map((option, index) => (
+              {/* {unit.map((option, index) => (
                 <>
                   <li key={index}>
                     {option.word_eng}
                   </li>
                 </>
-              ))}
+              ))} */}
+              <li>
+                {unit.verb1}
+              </li>
+              <li>
+                {unit.verb2}
+              </li>
+              <li>{unit.verb3}</li>
             </ul>
           </div>
         ))}
